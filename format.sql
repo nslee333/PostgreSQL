49bd4695f72f804c5a31c653c3297f2a1fc492df
Nathan <nslee333@gmail.com>
2022-10-06 22:29:17 -0700

    Added format.sql.

---
+++ format.sql
@@ -0,0 +1,1 @@
+insert into person (id, first_name, last_name, email, gender, date_of_birth, country_of_birth) values (9000, 'Nathan', 'MacMillian', 'hello@gmail.com', 'HELLO', '2022-02-01', 'Czech Republic');
