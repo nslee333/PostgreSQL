72f840c3eb665f2216b853d939d71b6255d29bd4
Nathan <nslee333@gmail.com>
2022-10-06 21:37:05 -0700

    Adding relations.

--- person-car.sql
+++ person-car.sql
@@ -1,3 +1,10 @@
+create table car (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    make VARCHAR(100) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    price NUMERIC(19, 2) NOT NULL
+);
+
 create table person (
     id BIGSERIAL NOT NULL PRIMARY KEY,
         first_name VARCHAR(50) NOT NULL,
@@ -5,15 +12,11 @@ create table person (
         gender VARCHAR(50) NOT NULL,
         email VARCHAR(100),
         date_of_birth DATE NOT NULL,
-        country_of_birth VARCHAR(50) NOT NULL
+        country_of_birth VARCHAR(50) NOT NULL,
+        car_id BIGINT REFRENCES car (id),
+        UNIQUE(car_id)
 );
 
-create table car (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    make VARCHAR(100) NOT NULL,
-    model VARCHAR(100) NOT NULL,
-    price NUMERIC(19, 2) NOT NULL
-)
 
 INSERT INTO person (first_name, last_name, gender, email, date_of_birth, country_of_birth) VALUES ('Fernanda', 'Beardon', 'Female', 'fb@gmail.com', '1953-10-28', 'Comoros');
 INSERT INTO person (first_name, last_name, gender, email, date_of_birth, country_of_birth) VALUES ('Omar', 'Colmore', 'Male', null, '1921-04-03', 'Finland');
