7d8d742af5aa289706d99608216a9629bdbe4974
Nathan <nslee333@gmail.com>
2022-10-12 16:36:50 -0700

    Init universe.sql, creating db + tables, inserting data into tables.

---
+++ universe/universe.sql
@@ -0,0 +1,107 @@
+CREATE DATABASE universe;
+
+CREATE TABLE galaxy(
+    galaxy_id BIGSERIAL PRIMARY KEY NOT NULL,
+    galaxy_name VARCHAR(50) UNIQUE NOT NULL,
+    galaxy_type VARCHAR(50) NOT NULL,
+    galaxy_constellation TEXT NOT NULL,
+    estimated_stars NUMERIC NOT NULL,
+    estimated_planets NUMERIC NOT NULL
+);
+
+CREATE TABLE star(
+    star_id BIGSERIAL PRIMARY KEY NOT NULL,
+    star_name VARCHAR(50) UNIQUE NOT NULL,
+    star_type VARCHAR NOT NULL,
+    galaxy_id BIGINT REFERENCES galaxy(galaxy_id) NOT NULL,
+    orbiting_planets NUMERIC NOT NULL
+);
+
+CREATE TABLE planet(
+    planet_id BIGSERIAL PRIMARY KEY NOT NULL,
+    planet_name VARCHAR(50) UNIQUE NOT NULL,
+    planet_type VARCHAR(50) NOT NULL,
+    star_id BIGINT REFERENCES star(star_id) NOT NULL,
+    planet_satellites_bool BOOLEAN,
+    planet_satellites NUMERIC NOT NULL,
+    planet_has_rings BOOLEAN,
+);
+
+CREATE TABLE moon(
+    moon_id BIGSERIAL PRIMARY KEY NOT NULL,
+    moon_name VARCHAR(50) UNIQUE NOT NULL,
+    planet_id BIGINT REFERENCES planet(planet_id) NOT NULL,
+    moon_orbital_speed_kms INT NOT NULL,
+    moon_orbit_time INT NOT NULL
+);
+
+CREATE TABLE black_hole(
+    bh_id BIGSERIAL PRIMARY KEY NOT NULL,
+    bh_name VARCHAR(50) UNIQUE NOT NULL,
+    bh_type VARCHAR(50) NOT NULL,
+    galaxy_id INT REFERENCES galaxy(galaxy_id),
+    bh_solar_mass NUMERIC NOT NULL
+);
+
+
+
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
+
+INSERT INTO star(star) VALUES();
+INSERT INTO star(star) VALUES();
+INSERT INTO star(star) VALUES();
+INSERT INTO star(star) VALUES();
+INSERT INTO star(star) VALUES();
+INSERT INTO star(star) VALUES();
+
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+INSERT INTO planet() VALUES();
+
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+INSERT INTO moon() VALUES();
+
+INSERT INTO black_hole() VALUES();
+INSERT INTO black_hole() VALUES();
+INSERT INTO black_hole() VALUES();
+INSERT INTO black_hole() VALUES();
+INSERT INTO black_hole() VALUES();
+INSERT INTO black_hole() VALUES();
+
+
+
+
+
+
