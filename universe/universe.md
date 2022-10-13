b1735e20e7e53116cff20c5c1d53c86207b53fa4
Nathan <nslee333@gmail.com>
2022-10-12 17:31:29 -0700

    Finished: - Building insert table sql, - universe.md formatting.

--- universe/universe.md
+++ universe/universe.md
@@ -45,51 +45,27 @@ Tables:
 Gather data here:
 
 	Galaxies x 6
-	Galaxy x 6
-			- galaxy_id (bigserial, pkey, not null), 
-			- galaxy_name (varchar, not null, UNIQUE),
-			- galaxy_type (varchar, not null),
-			- galaxy_constellation (TEXT, not null), 
-			- estimated_stars (numeric, NOT NULL), 
-			- estimated_planets (numeric, NOT NULL) 
-
-	- VALUES('Andromeda Galaxy', 'Barred Spiral Galaxy', 'Andromeda Constellation', 1 x 10^12, 1 x 10^9);
-	- VALUES('Milky Way Galaxy', 'Barred Spiral Galaxy', 'Sagittarius', 1 x 10^11, 1 X 10^11)
-	- VALUES('Triangulum Galaxy', 'Spiral Galaxy', 'Triangulum', 4 x 10^10, 4x10^10)
-	- VALUES ('Pinwheel Galaxy', 'Spirial Galaxy', 'Ursa Major', 1 x 10^12, 1 x 10^12)
-	- VALUES ('Black Eye Galaxy', 'Spirial Galaxy', 'Coma Berenices', 2 x 10^11, 2 x 10^11)
-	- VALUES ('Hoag's Object Galaxy', 'Ring Galaxy', 'Serpens Caput', 1 x 10^10, 1 x 10^10)
+	- VALUES('Andromeda Galaxy', 'Barred Spiral Galaxy', 'Andromeda Constellation', 1000000000000, 1000000000);
+	- VALUES('Milky Way Galaxy', 'Barred Spiral Galaxy', 'Sagittarius', 100000000000, 100000000000);
+	- VALUES('Triangulum Galaxy', 'Spiral Galaxy', 'Triangulum', 400000000000, 400000000000);
+	- VALUES ('Pinwheel Galaxy', 'Spirial Galaxy', 'Ursa Major', 1000000000000, 1000000000000);
+	- VALUES ('Black Eye Galaxy', 'Spirial Galaxy', 'Coma Berenices', 100000000000, 100000000000);
+	- VALUES ('Hoag's Object Galaxy', 'Ring Galaxy', 'Serpens Caput', 100000000000, 100000000000);
 
 
 
 
 	Stars x 6
-		- Star x 6
-			- star_id (bigserial, pkey, not null), 
-			- star_name (VARCHAR, not null, UNIQUE), 
-			- star_type (VARCHAR, not null), 
-			- galaxy_id (foreign key, REFERENCE relate to galaxy, not null), 
-			- orbiting_planets(numeric, not null), 
-
-	- VALUES('The Sun', 'G type', 2 (milky way), 8)
-	- VALUES('Kepler-11', 'Solar-type', 2 (milky way), 6)
-	- VALUES('Kepler-90', 'F-Type', 2, 8)
-	- VALUES('Proxima Centauri', 'Low-mass', 3, 3)
-	- VALUES('Tau Ceti', 'Solar-Type', 2, 8)
+	- VALUES('The Sun', 'G type', 2, 8);
+	- VALUES('Kepler-11', 'Solar-type', 2, 6);
+	- VALUES('Kepler-90', 'F-Type', 2, 8);
+	- VALUES('Proxima Centauri', 'Low-mass', 3, 3);
+	- VALUES('Tau Ceti', 'Solar-Type', 2, 8);
 	- VALUES('TRAPPIST', 'Red Dwarf', 2, 7);
 
 
 	Planets x 12
-	Planet x 12
-			- planet_id(bigserial, pkey, NOT NULL), 
-			- planet_name (varchar, UNIQUE, NOT NULL), 
-			- planet_type (composition, varchar, NOT NULL), 
-			- star_id(foreign key to Star, NOT NULL),
-			- planet_satelites_bool(boolean)
-			- planet_number_of_satellites(numeric, amount of moons)
-			- planet_has_rings(boolean)
-
-		'Jupiter', 'Gas Giant', '1', 'true', 80, false
+		'Jupiter', 'Gas Giant', 1, true, 80, false
 		'Saturn', 'Gas Giant', 1, true, 83, true
 		'Uranus', 'Ice Giant', 1, true, 27, false
 		'Neptune', 'Ice Giant', 1, true, 14, false
@@ -105,32 +81,26 @@ Gather data here:
 
 
 	Moons x 20
-	Moon x 20
-			- moon_id(bigserial, pkey, NOT NULL),
-			- moon_name(varchar, NOT NULL, UNIQUE),
-			- planet_id(REFERENCES planet_id)
-			- moon_orbital_speed_in_kms(INT, not null),
-			- moon_orbit_time_in_days(INT, not null),
-	
+
 	'Metis', 1, 1, 1
-	'Adrastea', 1, 31, 1,
+	'Adrastea', 1, 31, 1
 	'Amalthea', 1, 26, 1
-	'Thebe', 1, 23, 1,
+	'Thebe', 1, 23, 1
 	'Io', 1, 17, 2
 	'Europa', 1, 13, 4
 	'Ganymede', 1, 10, 7
 	'Callisto', 1, 8, 16
 	'Themisto', 1, 1, 129
-	'Leda', 1, 1, 242,
-	'Ersa', 1, 1, 252,
-	'Himalia', 1, 1, 248,
-	'Pandia', 1, 1, 252,
+	'Leda', 1, 1, 242
+	'Ersa', 1, 1, 252
+	'Himalia', 1, 1, 248
+	'Pandia', 1, 1, 252
 	'Lysithea', 1, 1, 258
-	'Elara', 1, 1, 258,
-	'Dia', 1, 1, 287,
-	'Carpo', 1, 1, 456,
-	'Valetudo', 1, 1, 500,
-	'Euporie', 1, 1, 550,
+	'Elara', 1, 1, 258
+	'Dia', 1, 1, 287
+	'Carpo', 1, 1, 456
+	'Valetudo', 1, 1, 500
+	'Euporie', 1, 1, 550
 	'Jupiter LV', 1, 1, 604
 
 	Black Holes x 6
@@ -141,9 +111,9 @@ Gather data here:
 			- galaxy_id(references, not null)
 			- bh_solar_mass(numeric, not null)
 
-		'TON 618', 'Quasar', 2, 6.6 x 10^10 
-		'Messier 31', 'Supermassive', 1, 1.2 x 10^11 
-		'4C', 'Binary', 3, 1.1 x 10^10
-		'Sagittarius A*', 2, 1.1 x 10^10
-		'NGC 5033', 5, 1.1 x 10^10
-		'RX J11', 6, 1.1 x 10^10
+		'TON 618', 'Quasar', 2, 66000000000
+		'Messier 31', 'Supermassive', 1, 120000000000
+		'4C', 'Binary', 3, 11000000000
+		'Sagittarius A*', 2, 11000000000
+		'NGC 5033', 5, 11000000000
+		'RX J11', 6, 11000000000
--- universe/universe.sql
+++ universe/universe.sql
@@ -45,57 +45,107 @@ CREATE TABLE black_hole(
 
 
 
-INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
-INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
-INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
-INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
-INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
-INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) VALUES();
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) 
+VALUES('Andromeda Galaxy', 'Barred Spiral Galaxy', 'Andromeda', 1000000000000, 1000000000);
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) 
+VALUES('Milky Way Galaxy', 'Barred Spiral Galaxy', 'Sagittarius', 100000000000, 100000000000);
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) 
+VALUES('Triangulum Galaxy', 'Spiral Galaxy', 'Triangulum', 400000000000, 400000000000);
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) 
+VALUES ('Pinwheel Galaxy', 'Spirial Galaxy', 'Ursa Major', 1000000000000, 1000000000000);
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) 
+VALUES ('Black Eye Galaxy', 'Spirial Galaxy', 'Coma Berenices', 100000000000, 100000000000);
+INSERT INTO galaxy(galaxy_name, galaxy_type, galaxy_constellation, estimated_stars, estimated_planets) 
+VALUES ('Hoag`s Object Galaxy', 'Ring Galaxy', 'Serpens Caput', 100000000000, 100000000000);
 
-INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) VALUES();
-INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) VALUES();
-INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) VALUES();
-INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) VALUES();
-INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) VALUES();
-INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) VALUES();
+INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) 
+VALUES('The Sun', 'G type', 2, 8);
+INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) 
+VALUES('Kepler-11', 'Solar-type', 2, 6);
+INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) 
+VALUES('Kepler-90', 'F-Type', 2, 8);
+INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) 
+VALUES('Proxima Centauri', 'Low-mass', 3, 3);
+INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) 
+VALUES('Tau Ceti', 'Solar-Type', 2, 8);
+INSERT INTO star(star_name, star_type, galaxy_id, orbiting_planets) 
+VALUES('TRAPPIST', 'Red Dwarf', 2, 7);
 
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
-INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) VALUES();
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Jupiter', 'Gas Giant', 1, true, 80, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Saturn', 'Gas Giant', 1, true, 83, true);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Uranus', 'Ice Giant', 1, true, 27, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Neptune', 'Ice Giant', 1, true, 14, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Mercury', 'Terrestrial', 1, false, 0, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Venus', 'Terrestrial', 1, false, 0, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Earth', 'Terrestrial', 1, true, 1, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Mars', 'Terrestrial', 1, true, 2, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Pluto', 'Dwarf', 1, true, 5, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Makemake', 'Dwarf', 1, true, 1, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('Haumea', 'Dwarf', 1, true, 2, false);
+INSERT INTO planet(planet_name, planet_type, star_id, planet_satellites_bool, planet_satellites, planet_has_rings) 
+VALUES('50000 Quaoar', 'Dwarf', true, 1, false);
 
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
-INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) VALUES();
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Metis', 1, 1, 1);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Adrastea', 1, 31, 1);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Amalthea', 1, 26, 1);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Thebe', 1, 23, 1);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Io', 1, 17, 2);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Europa', 1, 13, 4);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Ganymede', 1, 10, 7);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Callisto', 1, 8, 16);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Themisto', 1, 1, 129);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Leda', 1, 1, 242);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Ersa', 1, 1, 252);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Himalia', 1, 1, 248);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Pandia', 1, 1, 252);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Lysithea', 1, 1, 258);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Elara', 1, 1, 258);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Dia', 1, 1, 287);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Carpo', 1, 1, 456);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Valetudo', 1, 1, 500);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Euporie', 1, 1, 550);
+INSERT INTO moon(moon_name, planet_id, moon_orbital_speed_kms, moon_orbit_time) 
+VALUES('Jupiter LV', 1, 1, 604);
 
-INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) VALUES();
-INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) VALUES();
-INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) VALUES();
-INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) VALUES();
-INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) VALUES();
-INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) VALUES();
+INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) 
+VALUES('TON 618', 'Quasar', 2, 66000000000 );
+INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) 
+VALUES('Messier 31', 'Supermassive', 1, 120000000000);
+INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) 
+VALUES('4C', 'Binary', 3, 11000000000);
+INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) 
+VALUES('Sagittarius A*', 2, 11000000000);
+INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) 
+VALUES('NGC 5033', 5, 11000000000);
+INSERT INTO black_hole(bh_name, bh_type, galaxy_id, bh_solar_mass) 
+VALUES('RX J11', 6, 11000000000);
