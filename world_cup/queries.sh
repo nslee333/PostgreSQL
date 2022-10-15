d53de0e29b20d36cde513ea851c9015def769b5f
Nathan <nslee333@gmail.com>
2022-10-15 11:41:42 -0700

    Finished queries

--- world_cup/queries.sh
+++ world_cup/queries.sh
@@ -29,13 +29,14 @@ echo -e "\nWinner of the 2018 tournament team name:"
 echo "$($PSQL "SELECT name FROM teams WHERE team_id = (SELECT winner_id FROM games WHERE round = 'Final' AND year = '2018')")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT name FROM games FULL JOIN teams on games.winner_id = teams.team_id WHERE round = 'Eighth-Final' AND year = 2014 UNION SELECT name FROM games FULL JOIN teams on games.oppo
+nent_id = teams.team_id WHERE round = 'Eighth-Final' AND year = 2014 ORDER BY name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT DISTINCT name FROM games LEFT JOIN teams on games.winner_id = teams.team_id ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT year, name FROM games LEFT JOIN teams ON games.winner_id = teams.team_id WHERE round = 'Final' ORDER BY year;")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT * FROM teams WHERE name LIKE 'Co%'")"
