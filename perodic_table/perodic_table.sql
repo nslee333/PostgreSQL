f971e1cca8a9cbfa761d1ce94d631a745b071e99
Nathan <nslee333@gmail.com>
2022-10-28 15:57:39 -0700

    Initial commit for perodic_table project.

---
+++ perodic_table/perodic_table.sql
@@ -0,0 +1,108 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: periodic_table; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE periodic_table WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE periodic_table OWNER TO postgres;
+
+\connect periodic_table
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: elements; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.elements (
+    atomic_number integer NOT NULL,
+    symbol character varying(2),
+    name character varying(40)
+);
+
+
+ALTER TABLE public.elements OWNER TO freecodecamp;
+
+--
+-- Name: properties; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.properties (
+    atomic_number integer NOT NULL,
+    type character varying(30),
+    weight numeric(9,6) NOT NULL,
+    melting_point numeric,
+    boiling_point numeric
+);
+
+
+ALTER TABLE public.properties OWNER TO freecodecamp;
+
+--
+-- Name: elements elements_atomic_number_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.elements
+    ADD CONSTRAINT elements_atomic_number_key UNIQUE (atomic_number);
+
+
+--
+-- Name: elements elements_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.elements
+    ADD CONSTRAINT elements_pkey PRIMARY KEY (atomic_number);
+
+
+--
+-- Name: properties properties_atomic_number_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.properties
+    ADD CONSTRAINT properties_atomic_number_key UNIQUE (atomic_number);
+
+
+--
+-- Name: properties properties_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.properties
+    ADD CONSTRAINT properties_pkey PRIMARY KEY (atomic_number);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ perodic_table/todo.md
@@ -0,0 +1,11 @@
+1. fix the database.
+
+2. Create your git repo
+
+3. Create the script:
+    Script that accepts an arg in:
+        - atomic number
+        - symbol 
+        - or name
+    Then outputs some info about the given element
+    
