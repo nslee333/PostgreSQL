9ff3c7711c9e016a45303f35fcb76d533c0a6948
Nathan <nslee333@gmail.com>
2022-10-29 13:21:34 -0700

    Finished element.sh

--- periodic_table/element.sh
+++ periodic_table/element.sh
@@ -1,26 +1,62 @@
 #!/bin/bash
 
+SYM_REGEX='^[A-Z]+[a-z]*$'
+
+PSQL="psql --username=nslee333 --dbname=periodic_table -t -c"
+
+NAME_QUERY=$($PSQL "SELECT name FROM elements WHERE name='$1'");
+
+TRIMMED=${NAME_QUERY/ /}
 
 if [[ -z $1 ]]
 then
     echo "Please provide an element as an argument."
 
-# If $1 == atomic number
-    # query database for correct entry
-    # format string to echo what it wants.
-    # blah blah what it is etc  
+elif [[ $1 =~ [0-9] ]] 
+then
+    QUERY_RESULTS=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE atomic_number = $1")
 
-# if $1 == symbol of element
-    # blah blah
+    if [[ -n $QUERY_RESULTS ]]
+    then
+            echo $QUERY_RESULTS | while read TYPE_ID BAR ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR METAL_TYPE
+            do
+                echo "The element with the atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $METAL_TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+            done
+    else
+        echo "I could not find that element in the database."
 
-# IF $1 == name of element
-    # blah blah
+    fi
 
-# else 
-    # echo "I could not find that element in the database."
+elif [[ $1 =~ $SYM_REGEX ]] # Trying to get this to work.
+then
+    QUERY_RESULTS=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE symbol = '$1'")
 
+    if [[ -n $QUERY_RESULTS ]]
+    then
+            echo $QUERY_RESULTS | while read TYPE_ID BAR ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR METAL_TYPE
+            do
+                echo "The element with the atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $METAL_TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+            done
+    else
+        echo "I could not find that element in the database."
 
+    fi
 
+elif [[ "$1" == "$TRIMMED" ]]
+then
+    QUERY_RESULTS=$($PSQL "SELECT * FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE name = '$1'")
 
+    if [[ -n $QUERY_RESULTS ]]
+    then
+            echo $QUERY_RESULTS | while read TYPE_ID BAR ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR METAL_TYPE
+            do
+                echo "The element with the atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $METAL_TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+            done
+    else
+        echo "I could not find that element in the database. :)"
+        
 
+    fi
+else
+    echo "The argument provided was not correct: please enter an atomic number, symbol or name of the element you wish to know about."
 fi
