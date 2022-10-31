15b0ea80ae9ec0426e2a57d08cd5d7bafcf0f38f
Nathan <nslee333@gmail.com>
2022-10-31 10:20:35 -0700

    Wrote sql file for project. Wrote pseudo code for shell script.

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
 
+# Generate RAND_NUM
+
+# Prompt for a username:
+    # "Enter your username:"
+
+# if [[ username in database ]]
+# then
+    # echo "Welcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+
+# else
+    # echo "Welcome, $USER_NAME! It looks like this is your first time here."
+
+# echo "Guess the secret number between 1 and 1000:"
+#  while read USER_GUESS
+# do
+    # if [[ ! USER_GUESS =~ [0-1000] ]]
+    # then
+        # echo "That is not an integer, guess again:"
+        # continue
+    # elif [[ USER_GUESS < RAND_NUM ]]
+    # then 
+        # echo "It's higher than that, guess again:"
+        # continue
+    # elif [[ $USER_GUESS > RAND_NUM ]]
+    # then
+        # "It's lower than that, guess again:"
+        # continue
+    # elif [[ $USER_GUESS == RAND_NUM ]]
+    # then 
+        # echo "You guessed it in $NUM_GUESSES tries. The secret number was $RAND_NUM. Nice job!"
+        # break
+    # fi
+# done
---
+++ number_guessing_game/number_guess.sql
@@ -0,0 +1,2 @@
+CREATE TABLE players(user_id SERIAL PRIMARY KEY NOT NULL, user_name VARCHAR(50) NOT NULL, games_played INT, best_guess INT);
+
