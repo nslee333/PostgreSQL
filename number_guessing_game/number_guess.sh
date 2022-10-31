0cd095452349f1aca366fa7f9d4cd20411fbd7fb
Nathan <nslee333@gmail.com>
2022-10-31 15:58:18 -0700

    Small changes

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -12,14 +12,14 @@ then
     GAMES_PLAYED=$($PSQL "SELECT games_played FROM players WHERE user_name = '$USER_NAME'")
     BEST_GAME=$($PSQL "SELECT best_guess FROM players WHERE user_name = '$USER_NAME'")
 
-    echo "Welcome back, $USER_NAME! You have played '$GAMES_PLAYED' games, and your best game took '$BEST_GAME' guesses."
+    echo "Welcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 
     ((GAMES_PLAYED++))
     $PSQL "UPDATE players SET games_played = $GAMES_PLAYED WHERE user_name = '$USER_NAME'"
 else
     echo "Welcome, $USER_NAME! It looks like this is your first time here."
     
-    $PSQL "INSERT INTO players(user_name, games_played, best_guess) VALUES('$USER_NAME', 1, 50)"
+    $PSQL "INSERT INTO players(user_name, games_played, best_guess) VALUES('$USER_NAME', 1, 0)"
 fi
 
 RAND_NUM=$(( RANDOM % 1000 + 1 ))
@@ -47,7 +47,7 @@ do
     elif [[ $USER_GUESS == "$RAND_NUM" ]]
     then 
     echo "You guessed it in $NUM_GUESSES tries. The secret number was $RAND_NUM. Nice job!"
-        if [[ $NUM_GUESSES < $BEST_GAME ]]
+        if [[ $NUM_GUESSES < $BEST_GAME || $BEST_GAME == 0 ]]
         then
             $PSQL "UPDATE players SET best_guess = $NUM_GUESSES WHERE user_name = '$USER_NAME'"
         fi
